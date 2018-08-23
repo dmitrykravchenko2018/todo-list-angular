1a8c4d9eb60221d4c1cec1158a8e3bb344d45985
dmitrykravchenko <dmitrykravchenko95@gmail.com>
2018-08-23 23:18:01 +0300

    added raw components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { AddFormComponent } from './components/add-form/add-form.component';
+import { FilterFormComponent } from './components/filter-form/filter-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoItemComponent,
+    SideBarComponent,
+    AddFormComponent,
+    FilterFormComponent
   ],
   imports: [
     BrowserModule
